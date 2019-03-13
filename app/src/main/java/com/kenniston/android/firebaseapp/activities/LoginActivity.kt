ae2c41411d57f56ed4eb88d8bdd0717ea5e5a54e
Kenniston Arraes Bonfim <kenniston@gmail.com>
2019-03-13 00:18:46 -0300

    dependencies

---
+++ app/src/main/java/com/kenniston/android/firebaseapp/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.kenniston.android.firebaseapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kenniston.android.firebaseapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/kenniston/android/firebaseapp/activities/MainActivity.kt
+++ app/src/main/java/com/kenniston/android/firebaseapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kenniston.android.firebaseapp
+package com.kenniston.android.firebaseapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kenniston.android.firebaseapp.R
 
 class MainActivity : AppCompatActivity() {
 
